1821f0208dd7914db438684b5143797e52ac7c7d
jonatan <villegasjoncena7@gmail.com>
2023-05-31 21:40:49 -0600

    conection database mongodb

---
+++ database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import certifi
+
+#importando paquetes para la coneccion a mongo 
+MONGO_URI = 'mongodb+srv://jv2208674:#puteria @pythonflask.evxx3an.mongodb.net/?retryWrites=true&w=majority'
+ca = certifi.where()
+
+#Funcion para conectarse a mongo con los paquetes instalados 
+def conection_db():
+    try:
+        client = MongoClient.connet(MONGO_URI, tlsCAFILE = ca)
+        db = client['dbd_CRUD']
+    except ConnectionError:
+        print('Error al  conectarse')
+    return db
