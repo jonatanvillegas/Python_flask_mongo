5f598895899a1d4314a4a1a56a94b408f166fa43
jonatan <villegasjoncena7@gmail.com>
2023-06-07 22:30:56 -0600

    corrigiendo errores de conexion con la base de datos

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import certifi
 
 #importando paquetes para la coneccion a mongo 
-MONGO_URI = 'mongodb+srv://jv2208674:#puteria @pythonflask.evxx3an.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://jv2208674:#puteria@pythonflask.evxx3an.mongodb.net/?retryWrites=true&w=majority'
 ca = certifi.where()
 
 #Funcion para conectarse a mongo con los paquetes instalados 
