4a6e52dc8bc51b0b7016672a16c40f8290995f09
jonatan <villegasjoncena7@gmail.com>
2023-06-07 22:32:07 -0600

    elimiando esquema de producto y creandolo como diccionario desde el app.py

--- producto.py
+++ producto.py
@@ -1,15 +1,9 @@
 #creando la clase producto 
-
 class Producto:
     def __init__(self, name, description, price):
-        self.name = name,
-        self.description = description,
-        self.preci = price
+        self.name = name
+        self.description = description
+        self.price = price
         
-    #esquema de la entidad 
-    def EsquemaTablaProducto(self):
-        return{
-            'name' : self.name,
-            'description' : self.description,
-            'preci' : self.price
-        }
+ 
+   
